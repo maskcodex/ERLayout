d0a048898f8bb6092ec2bbc5dfabf58574ef5b99
vucodex <nkanhvu90@gmail.com>
2015-11-30 18:14:16 +0700

    update some css

--- js/index.js
+++ js/index.js
@@ -4,6 +4,25 @@ var hiddenDiv = $(document.createElement('div')),
 
 $(document).ready(function() {
   $('body').append(hiddenDiv);
+
+  $(".book-content-info-nav li").click(function(event) {
+  		var clicked = $(this);
+  		var current = clicked.attr('id');
+  	  $(".book-content-info-nav li").each(function() {
+		  $(this).removeClass("activated");
+		});
+
+  	  clicked.addClass("activated");
+
+  	  $(".book-content-info-content").each(function() {
+  	  	  console.log($(this).attr('for'));
+  	  	  if( $(this).attr('for') !== current) {
+  	  	  	$(this).hide();	
+  	  	  }else {
+  	  	  	$(this).slideDown(100);
+  	  	  }
+		});
+	});
 });
 
 function auto_grow(element) {
@@ -12,6 +31,5 @@ function auto_grow(element) {
     content = content.replace(/\n/g, '<br>');
     hiddenDiv.html(content + '<br class="lbr">');
     element.style.height = hiddenDiv.height() +'px';
-  
 }
 
